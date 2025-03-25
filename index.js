5deded4a8ee7f0bcd0465dc575f0d3b8f96028a4
hhgjada <43480702+jada04@users.noreply.github.com>
2025-03-24 23:03:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// const express = require('express');
-// const ytSearch = require('yt-search');
-// const cors = require('cors');
-// const ytdl = require('youtube-dl-exec');
+const express = require('express');
+const ytSearch = require('yt-search');
+const cors = require('cors');
+const ytdl = require('youtube-dl-exec');
 
 
 
@@ -41,64 +41,73 @@
 
 
 
-// app.get('/play', async (req, res) => {
-//   let url = req.query.url || '';
+app.get('/play', async (req, res) => {
+  let url = req.query.url || '';
 
-//   if (!url) {
-//     return res.status(400).json({ error: 'URL gerekli' });
-//   }
+  if (!url) {
+    return res.status(400).json({ error: 'URL gerekli' });
+  }
 
-//   // Eğer URL 'http' ile başlamıyorsa, video ID olarak kabul edip tam URL'ye dönüştürüyoruz.
-//   if (!url.startsWith('http')) {
-//     url = `https://www.youtube.com/watch?v=${url}`;
-//   }
-
-//   try {
-//     const output = await ytdl(url, {
-//       dumpJson: true,
-//       format: 'bestaudio',
-//       skipDownload: true,
-//       addHeader: [
-//         'referer: https://www.youtube.com/',
-//         'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
-//         'Accept: */*',
-//         'origin: https://www.youtube.com'
-//       ],
-//       // Cookie dosyanızın yolunu belirtin; bu dosyanın Docker imajınıza dahil edildiğinden emin olun.
-//       cookies: 'cookies.txt'
-//     });
-
-//     // Dönüşte output içindeki tüm verileri döndürmek isterseniz:
-//     res.json({
-//       title: output.title,
-//       audioUrl: output.url,
-      
-//     });
-//   } catch (error) {
-//     console.error('youtube-dl-exec ile hata:', error);
-//     res.status(500).json({ error: 'Video bilgisi alınamadı', details: error.message });
-//   }
-// });
+  // Eğer URL 'http' ile başlamıyorsa, video ID olarak kabul edip tam URL'ye dönüştürüyoruz.
+  if (!url.startsWith('http')) {
+    url = `https://www.youtube.com/watch?v=${url}`;
+  }
 
 
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-const youtubedl = require('youtube-dl-exec');
-
-youtubedl('https://www.youtube.com/watch?v=IfijOvGyWfA', {
+  youtubedl('https://www.youtube.com/watch?v=IfijOvGyWfA', {
   dumpSingleJson: true,
   noCheckCertificate: true,
   noWarnings: true,
   format: 'bestaudio'
 }).then(output => {
   console.log('Ses URL:', output.url);
+     res.json({
+      title: output.title,
+      audioUrl: output.url,
+      
+    });
 }).catch(error => {
   console.error('Hata:', error);
 });
+
+
+  
+
+  // try {
+  //   const output = await ytdl(url, {
+  //     dumpJson: true,
+  //     format: 'bestaudio',
+  //     skipDownload: true,
+  //     addHeader: [
+  //       'referer: https://www.youtube.com/',
+  //       'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
+  //       'Accept: */*',
+  //       'origin: https://www.youtube.com'
+  //     ],
+  //     // Cookie dosyanızın yolunu belirtin; bu dosyanın Docker imajınıza dahil edildiğinden emin olun.
+  //     cookies: 'cookies.txt'
+  //   });
+
+  //   // Dönüşte output içindeki tüm verileri döndürmek isterseniz:
+  //   res.json({
+  //     title: output.title,
+  //     audioUrl: output.url,
+      
+  //   });
+  // } catch (error) {
+  //   console.error('youtube-dl-exec ile hata:', error);
+  //   res.status(500).json({ error: 'Video bilgisi alınamadı', details: error.message });
+  // }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
 
 
 
